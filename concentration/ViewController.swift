5fc952dc46bb3bf36bea2175a4ca0a18521ef66b
Andres Vinueza <vinuean@c02ml55nfh00.local>
2019-04-22 09:58:55 -0400

    Initial Commit

---
+++ concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  concentration
+//
+//  Created by Vinueza, Andres on 4/22/19.
+//  Copyright © 2019 Vinueza, Andres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
